8d9e5b7911378d167fcb2beca43f56e88b173cda
Isaac Colon <tuj12239@temple.edu>
2020-09-16 18:49:12 -0400

    Added button with onClickListener which changes the TextView. All strings use XML resources.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button myButton = findViewById(R.id.button);
+        final TextView myText = findViewById(R.id.textView);
+
+        myButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                myText.setText(R.string.my_greeting2);
+            }
+        });
     }
+
+
 }
